6242c2290b37a54a83dd9ab9b0561139592138a1
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-10-06 23:00:10 +0200

    fix clips

--- app/clipPlayer.tsx
+++ app/clipPlayer.tsx
@@ -2,5 +2,5 @@ import { HelixClip } from '@twurple/api';
 
 export default function ClipPlayer(props: {clip: HelixClip}) {
     console.log(props.clip);
-    return (<iframe id="player" width="800" height="600" src={'https://player.twitch.tv/?video=' + props.clip.id + "&parent=" + encodeURI('nofuture2077.github.io')+ "&autoplay=false"} ></iframe>);
+    return (<iframe id="player" width="800" height="600" src={'https://player.twitch.tv/?video=' + props.clip.videoId + "&parent=" + encodeURI('nofuture2077.github.io')+ "&autoplay=true"} ></iframe>);
 }

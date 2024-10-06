d6ce0885908b19fa952a9e5e2e2a0ed4a15e0cdc
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-10-06 23:09:52 +0200

    fix embed

--- app/clipPlayer.tsx
+++ app/clipPlayer.tsx
@@ -2,5 +2,5 @@ import { HelixClip } from '@twurple/api';
 
 export default function ClipPlayer(props: {clip: HelixClip}) {
     console.log(props.clip);
-    return (<iframe id="player" width="800" height="600" src={props.clip.embedUrl + "?parent=" + encodeURI('nofuture2077.github.io')+ "&autoplay=true"} ></iframe>);
+    return (<iframe id="player" width="800" height="456" src={props.clip.embedUrl + "&parent=" + encodeURI('nofuture2077.github.io')+ "&autoplay=true"} ></iframe>);
 }
